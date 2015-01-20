2f51f770aa7e44f0ea1642f8a920fed6b61d7d6d
vollsate <vollsate@gmail.com>
2015-01-20 13:03:53 +0100

    Removed TODO stubs

--- src/no/glv/android/stdntworkflow/core/DBException.java
+++ src/no/glv/android/stdntworkflow/core/DBException.java
@@ -6,22 +6,18 @@ public class DBException extends RuntimeException {
     private static final long serialVersionUID = -5331569245421077680L;
 
     public DBException() {
-	// TODO Auto-generated constructor stub
     }
 
     public DBException( String detailMessage ) {
 	super( detailMessage );
-	// TODO Auto-generated constructor stub
     }
 
     public DBException( Throwable throwable ) {
 	super( throwable );
-	// TODO Auto-generated constructor stub
     }
 
     public DBException( String detailMessage, Throwable throwable ) {
 	super( detailMessage, throwable );
-	// TODO Auto-generated constructor stub
     }
 
 }
