9a6424db72e2dfc1cc94996d5130c371a37902b8
vollsate <vollsate@gmail.com>
2015-01-20 11:53:18 +0100

    Methodchange in Task

--- src/no/glv/android/stdntworkflow/TaskActivity.java
+++ src/no/glv/android/stdntworkflow/TaskActivity.java
@@ -224,7 +224,7 @@ public class TaskActivity extends BaseTabActivity {
 	    getDataHandler().commitStudentsTasks( mTask );
 	}
 
-	mTask.markAsUpdated();
+	mTask.markAsCommitted();
     }
 
     /**
