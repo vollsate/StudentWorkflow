18d07118041daa77b47903f9daa553d8131d52d8
vollsate <vollsate@gmail.com>
2015-01-28 18:07:53 +0100

    Enabled bit handling of which tasks to display

--- src/no/glv/android/stdntworkflow/InstalledTasksFragment.java
+++ src/no/glv/android/stdntworkflow/InstalledTasksFragment.java
@@ -3,6 +3,7 @@ package no.glv.android.stdntworkflow;
 import java.util.List;
 
 import no.glv.android.stdntworkflow.core.DataHandler.OnTasksChangedListener;
+import no.glv.android.stdntworkflow.intrfc.BaseValues;
 import no.glv.android.stdntworkflow.intrfc.Task;
 import android.content.Context;
 import android.content.Intent;
@@ -14,12 +15,40 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 /**
+ * This class will show any installed tasks in the system. You may specify
+ * certain arguments in order to control the output from this fragment.
+ * 
+ * <p>
+ * The following arguments are leagal: <blockquote>
+ * <table>
+ * <tr>
+ * <td><tt>Task.TASK_STATE_OPEN</tt></td>
+ * <td>Shows any open tasks in the system
+ * </tr>
+ * <tr>
+ * <td><tt>Task.TASK_STATE_CLOSED</tt></td>
+ * <td>Shows any closed tasks in the system
+ * </tr>
+ * <tr>
+ * <td><tt>Task.TASK_STATE_EXPIRED</tt></td>
+ * <td>Shows any expired in the system
+ * </tr>
+ * </table>
+ * </blockquote>
  * 
  * @author GleVoll
  *
  */
 public class InstalledTasksFragment extends InstalledDataFragment implements OnTasksChangedListener {
+	
+	/** This is used as a flag, and may contain none, one or all known states  */
+	public static final String PARAM_TASK_STATE = BaseValues.EXTRA_BASEPARAM + "taskState";
+	
+	private int taskState;
 
+	/**
+	 * 
+	 */
 	public InstalledTasksFragment() {
 		super();
 	}
@@ -27,8 +56,22 @@ public class InstalledTasksFragment extends InstalledDataFragment implements OnT
 	@Override
 	public void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
+		
+		if ( savedInstanceState != null ) {
+			taskState = savedInstanceState.getInt( PARAM_TASK_STATE );
+		}
+		else {
+			taskState = getArguments().getInt( PARAM_TASK_STATE );
+		}
 
 		dataHandler.addOnTaskChangeListener( this );
+	}
+	
+	@Override
+	public void onSaveInstanceState( Bundle outState ) {
+		super.onSaveInstanceState( outState );
+		
+		outState.putInt( PARAM_TASK_STATE, taskState );
 	}
 
 	@Override
@@ -38,7 +81,7 @@ public class InstalledTasksFragment extends InstalledDataFragment implements OnT
 
 	@Override
 	public List<String> getNames() {
-		return dataHandler.getTaskNames();
+		return dataHandler.getTaskNames( taskState );
 	}
 
 	@Override
@@ -87,5 +130,18 @@ public class InstalledTasksFragment extends InstalledDataFragment implements OnT
 	@Override
 	public void onTaskChange( Task newTask, int mode ) {
 		onDataChange( mode );
+	}
+
+	// -----------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------
+
+	public static interface TaskArguments {
+
+		public static final int TASK_ONLY_OPEN = 0;
 	}
 }
