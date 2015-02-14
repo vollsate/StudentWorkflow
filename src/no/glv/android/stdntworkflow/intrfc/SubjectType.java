e151acaec4d61a86c40aa2b9c68a14af3897e7b5
vollsate <vollsate@gmail.com>
2015-02-14 10:57:35 +0100

    Added comment and custom type

--- src/no/glv/android/stdntworkflow/intrfc/SubjectType.java
+++ src/no/glv/android/stdntworkflow/intrfc/SubjectType.java
@@ -1,9 +1,27 @@
 package no.glv.android.stdntworkflow.intrfc;
 
+/**
+ * Represents a subject or type to better specify the type of {@link Task} a
+ * {@link Student} is involved in.
+ * 
+ * <p>
+ * The two types are either <tt>subject</tt> or <tt>type</tt>. The subject may
+ * be translated to curriculum, and the type is category: text, parent approval
+ * or other types of tasks the student may be linked to.
+ * 
+ * <p>The system installs a few default subject/ types. Any custom type the user
+ * installs, gets a specific flag.
+ * 
+ * @author glevoll
+ *
+ */
 public interface SubjectType {
 
 	public static final int TYPE_TYPE = 1;
 	public static final int TYPE_SUBJECT = 2;
+	
+	/** Used to flag a custom type. */
+	public static final int TYPE_CUSTOM = 4;
 
 	public int getID();
 
